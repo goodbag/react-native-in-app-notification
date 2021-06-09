8b9332cc5a6e450ad419256c8aab641476b631f1
Johannes Herrnegger <johannes.herrnegger@gmail.com>
2021-06-09 14:56:16 +0200

    feat(WithInAppNotification): add forwardedRef

--- src/WithInAppNotification.hoc.js
+++ src/WithInAppNotification.hoc.js
@@ -5,26 +5,27 @@ import hoistNonReactStatic from 'hoist-non-react-statics';
 import Context from './Context';
 
 function withInAppNotification(WrappedComponent) {
-  class Enhanced extends React.PureComponent {
-    render() {
-      return (
-        <Context.Consumer>
-          {showNotification => (
-            React.forwardRef((props, ref) => (<WrappedComponent
-              {...this.props}
-              ref={ref}
-              showNotification={showNotification}
-            />))
-          )}
-        </Context.Consumer>
-      );
+    class Enhanced extends React.PureComponent {
+        render() {
+            const { forwardedRef, ...props } = this.props;
+            return (
+                <Context.Consumer>
+                    {showNotification => (
+                        <WrappedComponent
+                            {...this.props}
+                            ref={forwardedRef}
+                            showNotification={showNotification}
+                        />
+                    )}
+                </Context.Consumer>
+            );
+        }
     }
-  }
 
-  // Pass over static props
-  hoistNonReactStatic(Enhanced, WrappedComponent);
+    // Pass over static props
+    hoistNonReactStatic(Enhanced, WrappedComponent,);
 
-  return Enhanced;
+    return Enhanced;
 }
 
 export default withInAppNotification;
