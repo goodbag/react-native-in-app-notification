bc71a6fae1d7da3030035f81be95137388e476e3
Johannes Herrnegger <johannes.herrnegger@gmail.com>
2021-06-23 12:55:05 +0200

    feat: enable swiping

--- src/Notification.js
+++ src/Notification.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Animated, StyleSheet, Image } from 'react-native';
+import { Animated, StyleSheet, Image, Platform, Dimensions } from 'react-native';
 import { getStatusBarHeight, isIphoneX } from 'react-native-iphone-x-helper';
+import {
+    PanGestureHandler, State, PanGestureHandlerStateChangeEvent
+} from 'react-native-gesture-handler';
 
 import DefaultNotificationBody from './DefaultNotificationBody';
+
+const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
     notification: {
@@ -12,7 +17,18 @@ const styles = StyleSheet.create({
     },
 });
 
+const MIN_TRANSLATE_Y = -1000;
+const MAX_TRANSLATE_Y = 0;
+const MIN_TRANSLATE_X = 0;
+const MAX_TRANSLATE_X = 1000;
+const SWIPE_PIXELS_TO_CLOSE = 40;
+const SWIPE_ANIMATION_DURATION = 200;
+
 class Notification extends Component {
+    onPanGestureEvent;
+    translatePan;
+    swipeDirection = 'vertical';
+
     constructor() {
         super();
 
@@ -22,8 +38,40 @@ class Notification extends Component {
         // this.showNotification = this.showNotification.bind(this);
         this.closeNotification = this.closeNotification.bind(this);
 
+        if (Platform.OS === 'android') {
+            this.swipeDirection = 'horizontal';
+        }
+
+        this.translatePan = new Animated.Value(0);
+        this.translatePan.addListener((event) => {
+            console.log('translatePan', event);
+        })
+        let translatePanInterpolated;
+        if (this.swipeDirection === 'vertical') {
+            translatePanInterpolated = this.translatePan.interpolate({
+                inputRange: [MIN_TRANSLATE_Y, MAX_TRANSLATE_Y],
+                outputRange: [MIN_TRANSLATE_Y, MAX_TRANSLATE_Y],
+                extrapolate: 'clamp',
+            });
+            // translatePanInterpolated = this.translatePan;
+            this.onPanGestureEvent = Animated.event([{ nativeEvent: { translationY: this.translatePan } }], {
+                useNativeDriver: true,
+            });
+        } else {
+            translatePanInterpolated = this.translatePan.interpolate({
+                inputRange: [MIN_TRANSLATE_X, MAX_TRANSLATE_X],
+                outputRange: [MIN_TRANSLATE_X, MAX_TRANSLATE_X],
+                extrapolate: 'clamp',
+            })
+            this.onPanGestureEvent = Animated.event([{ nativeEvent: { translationX: this.translatePan } }], {
+                useNativeDriver: true,
+            });
+        }
+
         this.state = {
             animatedValue: new Animated.Value(0),
+            translatePan: this.translatePan,
+            translatePanInterpolated,
             isOpen: false,
         };
     }
@@ -38,25 +86,22 @@ class Notification extends Component {
 
     closeNotification() {
         const { onClose } = this.props;
-        if (onClose) {
-            Animated.timing(this.state.animatedValue, {
-                toValue: 0,
-                duration: this.props.openCloseDuration,
-                useNativeDriver: true,
-            }).start(() => {
-                if (onClose) {
-                    this.props.onClose();
-                }
-            });
-        }
-    };
+        console.log('[closeNotification] start onClose');
+        Animated.timing(this.state.animatedValue, {
+            toValue: 0,
+            duration: this.props.openCloseDuration,
+            useNativeDriver: true,
+        }).start(() => {
+            console.log('[closeNotification] onClose finished');
+            if (onClose) {
+                this.props.onClose();
+            }
+        });
+    }
 
     show() {
         const { closeInterval, title, message, onPress, icon, vibrate, additionalProps } = this.props;
         const { isOpen } = this.state;
-        // Clear any currently showing notification timeouts so the new one doesn't get prematurely
-        // closed
-        // clearTimeout(this.currentNotificationInterval);
         this.setState(
             {
                 isOpen: true,
@@ -85,21 +130,37 @@ class Notification extends Component {
             })
     }
 
-    // showNotification(done) {
-    //     // Animated.timing(this.state.animatedValue, {
-    //     //   toValue: 1,
-    //     //   duration: this.props.openCloseDuration,
-    //     //   useNativeDriver: true,
-    //     // }).start(done);
-    // }
+    onHandlerStateChange({ nativeEvent }) {
+        if (nativeEvent.state === State.ACTIVE) {
+            console.log('onHandlerStateChange nativeEvent.state === State.ACTIVE');
+        }
+        if (nativeEvent.oldState !== State.ACTIVE) {
+            console.log('onHandlerStateChange nativeEvent.oldState !== State.ACTIVE');
+            return;
+        }
+        console.log('onHandlerStateChange ---', nativeEvent.translationY);
 
-    //   closeNotification(done) {
-    //     // Animated.timing(this.state.animatedValue, {
-    //     //   toValue: 0,
-    //     //   duration: this.props.openCloseDuration,
-    //     //   useNativeDriver: true,
-    //     // }).start(done);
-    //   }
+        const swipePixelsToClose = SWIPE_PIXELS_TO_CLOSE;
+        let isSwipedOut;
+        if (this.swipeDirection === 'vertical') {
+            isSwipedOut = Math.abs(nativeEvent.translationY) > swipePixelsToClose;
+        }
+        else {
+            isSwipedOut = Math.abs(nativeEvent.translationX) > swipePixelsToClose;
+        }
+
+        if (isSwipedOut) {
+            this.closeNotification();
+        }
+        else {
+            Animated.timing(this.translatePan, {
+                toValue: this.swipeDirection === 'vertical' ? MAX_TRANSLATE_Y : 0,
+                easing: this.showParams?.swipeEasing,
+                duration: SWIPE_ANIMATION_DURATION,
+                useNativeDriver: true,
+            }).start();
+        }
+    }
 
     render() {
         const {
@@ -118,39 +179,58 @@ class Notification extends Component {
         } = this.props;
 
         const { animatedValue } = this.state;
+        let animatedPixels, animated, transform;
+        if (this.swipeDirection === 'vertical') {
+            animatedPixels = animatedValue.interpolate({
+                inputRange: [0, 1],
+                outputRange: [-height, 0],
+            });
+            animated = Animated.add(animatedPixels, this.state.translatePanInterpolated);
+            transform = [
+                {
+                    translateY: animated,
+                },
+            ];
+        } else {
+            animatedPixels = animatedValue.interpolate({
+                inputRange: [0, 1],
+                outputRange: [viewportWidth, 0],
+            });
+            animated = Animated.add(animatedPixels, this.state.translatePanInterpolated);
+            transform = [
+                {
+                    translateX: animated,
+                },
+            ];
+        }
 
         return (
-            <Animated.View
-                style={[
-                    styles.notification,
-                    { height, backgroundColor: backgroundColour },
-                    {
-                        transform: [
-                            {
-                                translateY: animatedValue.interpolate({
-                                    inputRange: [0, 1],
-                                    outputRange: [-height, 0],
-                                }),
-                            },
-                        ],
-                    },
-                    {
-                        opacity: animatedValue
-                    }
-                ]}
-            >
-                <NotificationBody
-                    title={title}
-                    message={message}
-                    onPress={onPress}
-                    isOpen={isOpen}
-                    iconApp={iconApp}
-                    icon={icon}
-                    vibrate={vibrate}
-                    onClose={() => this.setState({ isOpen: false }, this.closeNotification)}
-                    additionalProps={this.state.additionalProps}
-                />
-            </Animated.View>
+            <PanGestureHandler onGestureEvent={this.onPanGestureEvent} onHandlerStateChange={this.onHandlerStateChange.bind(this)}>
+                <Animated.View
+                    style={[
+                        styles.notification,
+                        { height, backgroundColor: backgroundColour },
+                        {
+                            transform: transform
+                        },
+                        {
+                            opacity: animatedValue
+                        }
+                    ]}
+                >
+                    <NotificationBody
+                        title={title}
+                        message={message}
+                        onPress={onPress}
+                        isOpen={isOpen}
+                        iconApp={iconApp}
+                        icon={icon}
+                        vibrate={vibrate}
+                        onClose={() => this.setState({ isOpen: false }, this.closeNotification)}
+                        additionalProps={this.state.additionalProps}
+                    />
+                </Animated.View>
+            </PanGestureHandler>
         );
     }
 }
